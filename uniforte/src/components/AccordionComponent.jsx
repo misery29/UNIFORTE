9fcbcf66ed7bc0830d48efcb81f89c24efdd4892
João Pedro <jaohds@gmail.com>
2024-06-09 19:53:04 -0300

    Benefícios e estilização do acordeão

--- uniforte/src/components/AccordionComponent.jsx
+++ uniforte/src/components/AccordionComponent.jsx
@@ -3,16 +3,12 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+import ExpandCircleDownOutlinedIcon from '@mui/icons-material/ExpandCircleDownOutlined';
 import styles from './AccordionComponent.module.css';
 
-import { styled } from '@mui/system';
-
-const BlackOutlinedWhiteBackgroundIcon = styled(ExpandCircleDownIcon)({
-  stroke:'black',
-  fill: 'white',
-});
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 
 export function AccordionComponent() {
     const [expanded, setExpanded] = useState(false);
@@ -25,37 +21,37 @@ export function AccordionComponent() {
         <div className={styles.accordionContainer}>
             <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} sx={{ border: '1px solid #ddd', mb: 1, boxShadow: 'none' }}>
                 <AccordionSummary
-                    expandIcon={<BlackOutlinedWhiteBackgroundIcon />}
+                    expandIcon={<ExpandCircleDownOutlinedIcon sx={{ fontSize: 32,color: '#000000', }} />}
                     aria-controls="panel1a-content"
                     id="panel1a-header"
-                    sx={{ borderBottom: '1px solid #ddd' }}
+                    sx={{ border: 'none',}}
                 >
                     <Typography>Nossa missão</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                    <Typography sx={{ fontSize: 20,color: '#000000', fontWeight: '400', }}>
                         Atender as expectativas dos nossos clientes, através de profissionais qualificados e prestar um excelente trabalho de forma transparente, tendo como ideal a lealdade e o sigilo absoluto, com nossas alianças empresariais e funcionários.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
             <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')} sx={{ border: '1px solid #ddd', mb: 1, boxShadow: 'none' }}>
                 <AccordionSummary
-                    expandIcon={<BlackOutlinedWhiteBackgroundIcon />}
+                    expandIcon={<ExpandCircleDownOutlinedIcon sx={{ fontSize: 32,color: '#000000', }}/>}
                     aria-controls="panel2a-content"
                     id="panel2a-header"
                     sx={{ borderBottom: '1px solid #ddd' }}
                 >
                     <Typography>Organização e Estrutura</Typography>
                 </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Trabalhamos na gestão das atividades-meios, possibilitando ao cliente dedicação e foco no seu próprio negócio. Fiscalização em todos os postos em que prestamos serviços. Avaliações periódicas de desempenho, boletins de visita e avaliação de qualidade de serviço prestado. Plantão permanente de reservas para atendimentos de substituição, com atendimento telefônico e sistema de rádio para contato com nossos supervisores e coordenadores. Treinamento e reciclagem de todos profissionais. Dispondo de viaturas para atendimento emergencial.
+                <AccordionDetails >
+                    <Typography sx={{ fontSize: 20,color: '#000000', fontWeight: '400', }}>
+                        Trabalhamos na gestão das atividades-meios, possibilitando ao cliente dedicação e foco no seu próprio negócio.<br></br><br></br>Fiscalização em todos os postos em que prestamos serviços.<br></br><br></br>Avaliações periódicas de desempenho, boletins de visita e avaliação de qualidade de serviço prestado.<br></br><br></br>Plantão permanente de reservas para atendimentos de substituição, com atendimento telefônico e sistema de rádio para contato com nossos supervisores e coordenadores.<br></br><br></br>Treinamento e reciclagem de todos profissionais.<br></br><br></br>Dispondo de viaturas para atendimento emergencial.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
             <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')} sx={{ border: '1px solid #ddd', mb: 1, boxShadow: 'none' }}>
                 <AccordionSummary
-                    expandIcon={<BlackOutlinedWhiteBackgroundIcon />}
+                    expandIcon={<ExpandCircleDownOutlinedIcon sx={{ fontSize: 32,color: '#000000', }}/>}
                     aria-controls="panel3a-content"
                     id="panel3a-header"
                     sx={{ borderBottom: '1px solid #ddd' }}
@@ -63,14 +59,14 @@ export function AccordionComponent() {
                     <Typography>Integridade e Transparência</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        Terceirizar não é apenas entregar à outra entidade a guarda de seu patrimônio. É, sobretudo: Ganhar com transferências das responsabilidades de toda possível ocorrência. Lucrar com a passagem das responsabilidades trabalhistas para o contratado. Para se livrar da co-responsabilidade, sobre as guias dos impostos mensalmente. Obter mais produtividade em seu próprio negócio.
+                    <Typography sx={{ fontSize: 20,color: '#000000', fontWeight: '400', }}>
+                        Terceirizar não é apenas entregar à outra entidade a guarda de seu patrimônio, é, sobretudo:<br></br><br></br> Obter mais produtividade em seu próprio negócio<br></br><br></br> Ganhar com transferências das responsabilidades de toda possível ocorrência.<br></br><br></br>Lucrar com a passagem das responsabilidades trabalhistas para o contratado, se livrando da co-responsabilidade, sobre as guias dos impostos mensalmente.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
             <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')} sx={{ border: '1px solid #ddd', mb: 1, boxShadow: 'none' }}>
                 <AccordionSummary
-                    expandIcon={<BlackOutlinedWhiteBackgroundIcon />}
+                    expandIcon={<ExpandCircleDownOutlinedIcon sx={{ fontSize: 32,color: '#000000', }}/>}
                     aria-controls="panel4a-content"
                     id="panel4a-header"
                     sx={{ borderBottom: '1px solid #ddd' }}
@@ -78,11 +74,11 @@ export function AccordionComponent() {
                     <Typography>Valores</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        » Trabalhar em equipe.
-                        » Colocando as necessidades de nossos clientes em primeiro plano.
-                        » Conduzindo as nossas ações de forma íntegra, conforme a moral e a ética.
-                        » Estamos em desenvolvimento em meio ao habitat empreendedor!
+                    <Typography sx={{ fontSize: 20,color: '#000000', fontWeight: '400', }}>
+                        Trabalhar em equipe.<br></br><br></br>
+                        Colocando as necessidades de nossos clientes em primeiro plano.<br></br><br></br>
+                        Conduzindo as nossas ações de forma íntegra, conforme a moral e a ética.<br></br><br></br>
+                        Estamos em desenvolvimento em meio ao habitat empreendedor!
                     </Typography>
                 </AccordionDetails>
             </Accordion>
---
+++ uniforte/src/components/Beneficios.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './Beneficios.module.css';
+import TrendingDownOutlinedIcon from '@mui/icons-material/TrendingDownOutlined';
+import CrisisAlertOutlinedIcon from '@mui/icons-material/CrisisAlertOutlined';
+import TrendingUpOutlinedIcon from '@mui/icons-material/TrendingUpOutlined';
+import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
+import HeightOutlinedIcon from '@mui/icons-material/HeightOutlined';
+import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
+
+
+export function Beneficios() {
+    return (
+        <div className={styles.container}>
+            <p className={styles.description}>
+                Descubra como nossos serviços podem transformar a gestão do seu negócio,<br></br>
+                proporcionando a tranquilidade e a eficiência que você merece.
+            </p>
+            <div className={styles.benefitsGrid}>
+                <div className={styles.benefit}>
+                    <div className={styles.icon}><TrendingDownOutlinedIcon sx={{ fontSize: 40,color: '#000000', }}/></div>
+                    <p>Redução de Custos</p>
+                </div>
+                <div className={styles.benefit}>
+                    <div className={styles.icon}><HeightOutlinedIcon sx={{ fontSize: 40,color: '#000000', }}/></div>
+                    <p>Flexibilidade e Escalabilidade</p>
+                </div>
+                <div className={styles.benefit}>
+                    <div className={styles.icon}><CrisisAlertOutlinedIcon sx={{ fontSize: 36,color: '#000000', }}/></div>
+                    <p>Foco no Core Business</p>
+                </div>
+                <div className={styles.benefit}>
+                    <div className={styles.icon}><RemoveCircleOutlineOutlinedIcon sx={{ fontSize: 40,color: '#000000', }}/></div>
+                    <p>Redução de Riscos</p>
+                </div>
+                <div className={styles.benefit}>
+                    <div className={styles.icon}><SchoolOutlinedIcon sx={{ fontSize: 40,color: '#000000', }}/></div>
+                    <p>Acesso a Especialistas</p>
+                </div>
+                <div className={styles.benefit}>
+                    <div className={styles.icon}><TrendingUpOutlinedIcon sx={{ fontSize: 40,color: '#000000', }}/></div>
+                    <p>Maior Eficiência Operacional</p>
+                </div>
+            </div>
+        </div>
+    );
+}
