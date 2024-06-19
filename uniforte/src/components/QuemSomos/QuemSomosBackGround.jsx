e72f4d33b2167fa0366e83a731619174604faa51
João Pedro <jaohds@gmail.com>
2024-06-18 22:42:14 -0300

    Ajustes na responsividade da aba QuemSomos

--- uniforte/src/components/QuemSomos/QuemSomosBackGround.jsx
+++ uniforte/src/components/QuemSomos/QuemSomosBackGround.jsx
@@ -3,7 +3,7 @@ import styles from './QuemSomos.module.css';
 export default function QuemSomosBackgroundSVG() {
     return (
     <div classname={styles.SVGContainer}>
-    <div className= {styles.svg_QuemSomos}>
+    <div className={styles.svg_QuemSomos}>
     <svg width="2000" height="1800" viewBox="0 0 1440 1185" xmlns="http://www.w3.org/2000/svg">
         <path d="M1702.32 -227.761L1503 1191L-235 1191L1702.32 -227.761Z" fill="#2e3f94"/>
     </svg>
