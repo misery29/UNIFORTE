e2c4fedf4a7358fbbd3e138daa431e8ce049ee09
João Pedro <jaohds@gmail.com>
2024-06-20 11:26:38 -0300

    mudanças no footer

--- uniforte/src/components/Footer/Footer.jsx
+++ uniforte/src/components/Footer/Footer.jsx
@@ -6,19 +6,20 @@ export function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={styles.footer_content}>
-                <div className={styles.logo_section}>
-                    <img src={Logo} alt="Logo da Empresa" className={styles.logo}/>
-                </div>
+
                 <div className={styles.links_section}>
                     <ul>
+                    <li>
+                    <img src={Logo} alt="Logo da Empresa" className={styles.logo}/>
+                    </li>
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">Sobre</a></li>
                         <li><a href="#services">Serviços</a></li>
                         <li><a href="#contact">Contato</a></li>
                     </ul>
-                </div>
-                <div className={styles.copyright_section}>
-                    <p>&copy; 2024 Sua Empresa. Todos os direitos reservados.</p>
+                    <div className={styles.line}></div>
+                </div>                <div className={styles.copyright_section}>
+                    <p>&copy; Uniforte serviços terceirizados<br></br>Todos os direitos reservados.</p>
                 </div>
             </div>
         </footer>
