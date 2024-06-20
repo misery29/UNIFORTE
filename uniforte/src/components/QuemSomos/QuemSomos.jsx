969f93e4842c45ad5093fb6a2db82b979374590e
gbastosvidal@gmail.com <gbastosvidal@gmail.com>
2024-06-20 16:05:48 -0300

    ajustes mobile

--- uniforte/src/components/QuemSomos/QuemSomos.jsx
+++ uniforte/src/components/QuemSomos/QuemSomos.jsx
@@ -10,23 +10,39 @@ import QuemSomosImg from "../../assets/QuemSomosImg.png";
 export function QuemSomos() {
     return (
         <div className={styles.container}>
-            <div className={styles.content}>
-                <h1>QUEM<br></br>SOMOS<br></br>NÓS</h1>
-                <p>
-                    A UNIFORTE, dedica-se à prestação de serviços terceirizados, provendo a melhor composição de serviços e tecnologias para atender as demandas de nossos clientes, minimizando riscos e custos com planejamento para aliarmos qualidade, rapidez e preço justo.
-                </p>
-                <br></br>
-                <br></br>
-                <p>
-                    Proporcionamos sempre a excelência em soluções e qualidade no atendimento, cumprindo nossas obrigações contratuais, fiscais e trabalhistas.
-                </p>
+            <div className={styles.desktop}>
+                    <div className={styles.content}>
+                        <h1>QUEM<br></br>SOMOS<br></br>NÓS</h1>
+                        <p>
+                            A UNIFORTE, dedica-se à prestação de serviços terceirizados, provendo a melhor composição de serviços e tecnologias para atender as demandas de nossos clientes, minimizando riscos e custos com planejamento para aliarmos qualidade, rapidez e preço justo.
+                        </p>
+                        <br></br>
+                        <br></br>
+                        <p>
+                            Proporcionamos sempre a excelência em soluções e qualidade no atendimento, cumprindo nossas obrigações contratuais, fiscais e trabalhistas.
+                        </p>
+                    </div>
+                    <img src={QuemSomosImg} className={styles.QuemSomosImg}></img>
+                    <div className={styles.accordion}>
+                        <AccordionComponent />
+                    </div>
+                <div className={styles.background}>
+                    <BackgroundSVG />
+                </div>
             </div>
-                <img src={QuemSomosImg} className={styles.QuemSomosImg}></img>
+            
+            <div className={styles.mobile}>
+                <div className={styles.content_mobile}>
+                    <h1>QUEM SOMOS NÓS</h1>
+                        <p>
+                        A UNIFORTE, dedica-se à prestação de serviços terceirizados, provendo a melhor composição de serviços e tecnologias para atender as
+                        demandas de nossos clientes, minimizando riscos e custos com planejamento para aliarmos qualidade, rapidez e preço justo. <br/>
+                        Proporcionamos sempre a excelência em soluções e qualidade no atendimento, cumprindo nossas obrigações contratuais, fiscais e trabalhistas.
+                        </p>
+                </div>
                 <div className={styles.accordion}>
                     <AccordionComponent />
                 </div>
-            <div className={styles.background}>
-                <BackgroundSVG />
             </div>
         </div>
     );
